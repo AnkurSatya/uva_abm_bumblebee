b3d307a59bfee3e115312c5b0ba21c6fa110730a
AndreaMaestri18 <andreamaestrimaestri@gmail.com>
2022-02-08 15:34:34 +0100

    results for best points in each bin

--- bumblebee_evolution/run.py
+++ bumblebee_evolution/run.py
@@ -6,10 +6,10 @@ import pickle
 allData = []
 
 for i in range(50):
-    model = BeeEvolutionModel(forager_royal_ratio=0.2, growth_factor=0.5, resource_variability=0.25, seed=i)
+    model = BeeEvolutionModel(forager_royal_ratio=0.05, growth_factor=0.05, resource_variability=0.05, seed=i)
     model.run_model()
     data = model.datacollector.get_model_vars_dataframe()
     allData.append(data)
 
-with open(f"results/50_runs_best.pickle", 'wb') as f:
+with open(f"results/50_runs_worst_low.pickle", 'wb') as f:
 		pickle.dump(allData, f)
