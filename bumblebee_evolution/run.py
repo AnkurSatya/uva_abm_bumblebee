72456357f1381413ad4b9d3f1c52cc6098fe68cf
Ottis1 <otto.kuusela@outlook.com>
2022-02-10 18:33:10 +0100

    Clean up run.py

--- bumblebee_evolution/run.py
+++ bumblebee_evolution/run.py
@@ -1,29 +1,25 @@
 from model import BeeEvolutionModel
 import matplotlib.pyplot as plt
-from agents import *
-import pickle
 
 '''
-In the following file, multiple runs of the model are run in order to produce stats.
-Each singlue run use a different seed.
+This script runs a single simulation and presents the results graphically.
 '''
 
-allData = []
-number_of_runs = 1
-for i in range(number_of_runs):
-    model = BeeEvolutionModel(forager_royal_ratio=0.55, growth_factor=0.85, resource_variability=0.25, seed=i, daily_data_collection=True)
-    model.run_model()
-    data = model.datacollector.get_model_vars_dataframe()
-    allData.append(data)
+model = BeeEvolutionModel(forager_royal_ratio=0.55, growth_factor=0.85, resource_variability=0.05, seed=1, daily_data_collection=True)
+model.run_model()
+data = model.datacollector.get_model_vars_dataframe()
 
-fig, ax = plt.subplots(nrows=2+model.num_hives)
+fig, ax = plt.subplots(nrows=1, ncols=2+model.num_hives, figsize=(20,4))
 data[['Total Workers', 'Total Queens', 'Total Drones']].plot(ax=ax[0])
 data[['Total Fertilized Queens']].plot(ax=ax[1])
 for i in range(model.num_hives):
-    data[[f'Workers in Hive {i}', f'Queens in Hive {i}', f'Drones in Hive {i}']].plot(ax=ax[i+2])
-for a in ax:
-    a.grid()
-plt.show()
+    data[[f'Workers in Hive {i}', f'Queens in Hive {i}', f'Drones in Hive {i}']].plot(ax=ax[i+2], legend=False)
+    ax[i+2].set_title(f"Hive {i+1}")
+    ax[i+2].legend(('Workers', 'Queens', 'Drones'))
 
-# with open(f"results/50_runs_bestOverall_low.pickle", 'wb') as f:
-# 		pickle.dump(allData, f)
+for a in ax:
+    a.set_xlabel('Days')
+    a.set_ylabel('Count')
+    a.grid()
+
+plt.show()
